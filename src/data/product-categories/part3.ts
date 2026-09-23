f84e3d23014aaccb910a2b7626c6148c0c508d0f
BBPweb <art.bbp10@gmail.com>
2026-09-23 11:38:19 +0530

    Add catalogue product data part 3

---
+++ src/data/product-categories/part3.ts
@@ -0,0 +1,82 @@
+import type { ProductCategory } from './types';
+
+export const productCategoriesPart3: ProductCategory[] = [
+  {
+    "name":"CANNED FOODS","slug":"canned-foods","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"KINGSWAY COCONUT MILK","weight":"400G"},{"name":"KINGSWAY MANGO PULP","weight":"850G"},
+      {"name":"KINGSWAY CHICKPEAS","weight":"400G"},{"name":"KINGSWAY KALACHANA","weight":"400G"},
+      {"name":"KINGSWAY BUTTER BEANS","weight":"400G"},{"name":"KINGSWAY GUNGO PEAS","weight":"400G"},
+      {"name":"KINGSWAY RED KIDNEY BEANS","weight":"400G"}
+    ]
+  },
+  {
+    "name":"RICE | POHA | MAMRA","slug":"rice-poha-mamra","image":"/images/Rice.jpeg","products":[
+      {"name":"MEDIUM POWA","weight":"1KG"},{"name":"THICK POWA","weight":"1KG"},{"name":"MAKAI POWA","weight":"15KG"},
+      {"name":"KANAIYA MAMRA","weight":"500G"},{"name":"KINGSWAY PHOOL MAKANA","weight":"100G"}
+    ]
+  },
+  {
+    "name":"GHEE | OIL","slug":"ghee-oil","image":"/images/Edibleoilsghee.jpeg","products":[
+      {"name":"KINGSWAY NATURAL VIRGIN COCONUT OIL","weight":"1LTR"},{"name":"KINGSWAY NATURAL VIRGIN COCONUT OIL","weight":"200ML"},
+      {"name":"KINGSWAY NATURAL VIRGIN COCONUT OIL","weight":"500ML"},{"name":"KINGSWAY NATURAL VIRGIN COCONUT OIL","weight":"1LTR"},
+      {"name":"KINGSWAY NATURAL VIRGIN COCONUT OIL","weight":"2LTR"},{"name":"KINGSWAY SUNFLOWER OIL","weight":"5LTR"},
+      {"name":"SWARNAM NATURAL SESAME OIL","weight":"500ML"},{"name":"SWARNAM NATURAL SESAME OIL","weight":"1LTR"},
+      {"name":"KHYBER GHEE","weight":"12.5KG"},{"name":"LAKES BUTTER GHEE PURE TIN","weight":"500G"},
+      {"name":"LAKES BUTTER GHEE PURE TUB","weight":"1KG"},{"name":"LAKES BUTTER GHEE PURE TUB","weight":"2KG"},
+      {"name":"LAKES BUTETR GHEE PURE TIN","weight":"2KG"}
+    ]
+  },
+  {
+    "name":"PASTE | SAUCES","slug":"paste-sauces","image":"/images/Ready.jpeg","products":[
+      {"name":"MAGGI HOT & SWEET SAUCE","weight":"400G"},{"name":"MAGGI CHILLI GARLIC SAUCE","weight":"305G"},
+      {"name":"MAGGI CHILLI SAUCE","weight":"340G"},{"name":"MAGGI MASALA CHILLI SAUCE","weight":"400G"},
+      {"name":"MAGGI EX HOT CHILLI SAUCE","weight":"320G"},{"name":"MAGGI TAMARIND SAUCE","weight":"380G"}
+    ]
+  },
+  {
+    "name":"PAPAD | NOODLES","slug":"papad-noodles","image":"/images/Ready.jpeg","products":[
+      {"name":"KINGSWAY MADRAS PAPPAD","weight":"200G"},{"name":"KINGSWAY PLAIN PAPPADUM","weight":"200G"},{"name":"MAGGI NOODLES","weight":"70G"}
+    ]
+  },
+  {
+    "name":"INCENSE STICK (AGARBATTI)","slug":"incense-stick-agarbatti","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"RAAZI AGARBATTI FRESH","weight":"6PCS"},{"name":"RAAZI AGARBATTI GUGGAL","weight":"6PCS"},
+      {"name":"RAAZI AGARBATTI CHANDAN DHOOP","weight":"6PCS"},{"name":"RAAZI AGARBATTI LOBAN","weight":"6PCS"},
+      {"name":"RAAZI AGARBATTI MOGRA","weight":"6PCS"},{"name":"RAAZI AGARBATTI ROSE","weight":"6PCS"},
+      {"name":"RAAZI AGARBATTI SAI BABA","weight":"6PCS"},{"name":"RAAZI AGARBATTI SANDAL","weight":"6PCS"},
+      {"name":"RAAZI AGARBATTI AARTI","weight":"6PCS"},{"name":"RAAZI AGARBATTI PHOOL KE DARSHAN","weight":"6PCS"},
+      {"name":"RAAZI AGARBATTI VARIETY KE DARSHAN","weight":"6PCS"},{"name":"RAAZI AGARBATTI BHARAT KE DARSHAN","weight":"6PCS"},
+      {"name":"RAAZI AGARBATTI ASSORTED","weight":"6PCS"},{"name":"RAAZI AGABATTI SUGHAND SRINGAR","weight":"6PCS"},
+      {"name":"RAAZI METRO MELON","weight":"6PCS"}
+    ]
+  },
+  {
+    "name":"GRACE BEVERAGES","slug":"grace-beverages","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"GRACE MIGHTY MALT - GLASS","weight":"330ML"},{"name":"GRACE MIGHTY MALT - GLASS 6-PACK","weight":"330ML"}
+    ]
+  },
+  {
+    "name":"GRACE COCONUT PRODUCTS","slug":"grace-coconut-products","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"GRACE COCONUT MILK","weight":"400ML"}
+    ]
+  },
+  {
+    "name":"GRACE SOUPS","slug":"grace-soups","image":"/images/Ready.jpeg","products":[
+      {"name":"GRACE COCK FLAVOUR SOUP MIX","weight":"50G"},{"name":"GRACE CHICKEN FLAVOUR NOODLE SOUP MIX","weight":"60G"},
+      {"name":"GRACE PUMPKIN SOUP MIX","weight":"50G"},{"name":"GRACE FISH TEA SOUP MIX","weight":"50G"}
+    ]
+  },
+  {
+    "name":"DUNN'S RIVER DRIED POWDERS & PULSES","slug":"dunns-river-dried-powders-pulses","image":"/images/Pulseslentilsjpeg.jpeg","products":[
+      {"name":"DUNN'S RIVER CORNMEAL COARSE","weight":"500G"},{"name":"DUNN'S RIVER CORNMEAL FINE","weight":"500G"},
+      {"name":"DUNN'S RIVER CORNMEAL COARSE","weight":"1.5KG"},{"name":"DUNN'S RIVER CORNMEAL FINE","weight":"1.5KG"},
+      {"name":"DUNN'S RIVER RED KIDNEY BEANS","weight":"500G"}
+    ]
+  },
+  {
+    "name":"BRUNSWICK SARDINES","slug":"brunswick-sardines","image":"/images/Ready.jpeg","products":[
+      {"name":"BRUNSWICK SARDINES IN SOYA OIL","weight":"106G"},{"name":"BRUNSWICK SARDINES WITH HOT PEPPERS","weight":"106G"},
+      {"name":"BRUNSWICK SARDINES IN LOUISIANA HOT SAUCE","weight":"106G"},{"name":"BRUNSWICK SARDINES IN TOMATO SAUCE","weight":"106G"}
+    ]
+  }
+];
