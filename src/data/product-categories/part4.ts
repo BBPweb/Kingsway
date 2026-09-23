4c6263f6730d49566f5bac1227783d9a72be2d75
BBPweb <art.bbp10@gmail.com>
2026-09-23 11:38:49 +0530

    Add catalogue product data part 4

---
+++ src/data/product-categories/part4.ts
@@ -0,0 +1,83 @@
+import type { ProductCategory } from './types';
+
+export const productCategoriesPart4: ProductCategory[] = [
+  {
+    "name":"FLOUR","slug":"flour","image":"/images/Floursgrains.jpeg","products":[
+      {"name":"SWARNAM RICE POWDER","weight":"500G"},{"name":"SWARNAM RICE POWDER","weight":"1KG"},{"name":"SWARNAM RICE POWDER","weight":"5KG"},
+      {"name":"SWARNAM PUTTU PODI","weight":"500G"},{"name":"SWARNAM PUTTU PODI","weight":"1KG"},{"name":"SWARNAM PUTTU PODI","weight":"5KG"},
+      {"name":"EUROSTAR CORN FLOUR","weight":"3KG"},{"name":"GENTEEL GRAM FLOUR","weight":"2KG"},
+      {"name":"EUROSTAR SAMOLINA COARSE","weight":"25KG"},{"name":"EUROSTAR SAMOLINA FINE","weight":"25KG"},
+      {"name":"TALOD KHAMAN","weight":"500G"},{"name":"CHAMPION NUMBER 1 ATTA","weight":"25KG"},{"name":"CHAMPION NUMBER 1 ATTA","weight":"10KG"},
+      {"name":"CHAMPION NUMBER 2 ATTA","weight":"25KG"},{"name":"CHAMPION NUMBER 2 ATTA","weight":"10KG"},
+      {"name":"CHAMPION PLAIN FLOUR","weight":"25KG"},{"name":"VICTORIA SELF RAISING FLOUR","weight":"25KG"},{"name":"IWISA","weight":"10KG"}
+    ]
+  },
+  {
+    "name":"NUTS","slug":"nuts","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"KINGSWAY CASHEW","weight":"700G"},{"name":"KINGSWAY CASHEW SPLIT","weight":"700G"},
+      {"name":"KINGSWAY PISTACHIO KERNALS","weight":"700G"},{"name":"KINGSWAY GREEN RAISINS","weight":"700G"},
+      {"name":"CASHEW SPLT 48LBS","weight":"21.77KG"},{"name":"GREEN RAISIN","weight":"12.5KG"},{"name":"GOLDEN RAISIN","weight":"12.5KG"},
+      {"name":"KINGSWAY DESICATED COCONUT MEDIUM","weight":"300G"},{"name":"KINGSWAY DESICATED COCONUT MEDIUM","weight":"700G"},
+      {"name":"KINGSWAY DESICATED COCONUT FINE","weight":"300G"},{"name":"KINGSWAY DESICATED COCONUT FINE","weight":"700G"},
+      {"name":"KINGSWAY DESICATED COCONUT MEDIUM","weight":"25KG"},{"name":"KINGSWAY DESICATED COCONUT FINE","weight":"25KG"}
+    ]
+  },
+  {
+    "name":"JAGGERY | SALT | SUGAR","slug":"jaggery-salt-sugar","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"AUTHENTIC JAGGERY","weight":"900G"},{"name":"AUTHENTIC JAGGERY","weight":"9KG"},
+      {"name":"PREMIUM JAGGERY","weight":"900G"},{"name":"PREMIUM JAGGERY","weight":"1.8KG"},{"name":"PREMIUM JAGGERY","weight":"4.5KG"},
+      {"name":"JAGGERY POWDER","weight":"500G"},{"name":"TATA SALT","weight":"1KG"},{"name":"EXPRESS SALT","weight":"10KG"},{"name":"T&L GRANULE SUGAR","weight":"25KG"}
+    ]
+  },
+  {
+    "name":"TOILETRIES","slug":"toiletries","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"MYSORE SANDAL","weight":"75G"},{"name":"DISH GOLD LIQUID 250ML","weight":"250ML"},
+      {"name":"DISH GOLD LIQUID 500ML","weight":"500ML"},{"name":"DG DRESS WASH","weight":"2KG"}
+    ]
+  },
+  {
+    "name":"ENCONA SAUCES","slug":"encona-sauces","image":"/images/Ready.jpeg","products":[
+      {"name":"ENCONA WEST INDIAN ORIGINAL HOT PEPPER SAUCE","weight":"142ML"},
+      {"name":"ENCONA WEST INDIAN PAPAYA HOT PEPPER SAUCE","weight":"142ML"},
+      {"name":"ENCONA JAMAICAN STYLE JERK BBQ SAUCE","weight":"142ML"},
+      {"name":"ENCONA THAI SWEET CHILLI SAUCE","weight":"142ML"},
+      {"name":"ENCONA INDIAN MANGO CHILLI SAUCE","weight":"142ML"},
+      {"name":"ENCONA SRIRACHA SAUCE","weight":"142ML"},
+      {"name":"ENCONA LOUISIANA CAJUN HOT SAUCE","weight":"142ML"},
+      {"name":"ENCONA WEST INDIAN EXXXTRA HOT PEPPER SAUCE","weight":"142ML"},
+      {"name":"ENCONA AFRICAN PERI PERI SAUCE","weight":"142ML"},
+      {"name":"ENCONA SOUTH CAROLINA REAPER SAUCE","weight":"142ML"},
+      {"name":"ENCONA WEST INDIAN ORIGINAL HOT PEPPER SAUCE","weight":"220ML"},
+      {"name":"ENCONA THAI SWEET CHILLI SAUCE","weight":"220ML"},
+      {"name":"ENCONA JAMAICAN SCOTCH BONNET HOT PEPPER SAUCE","weight":"220ML"}
+    ]
+  },
+  {
+    "name":"GRACE FISH & MEAT PRODUCTS","slug":"grace-fish-meat-products","image":"/images/Ready.jpeg","products":[
+      {"name":"GRACE MACKEREL IN BRINE - CHUNKY","weight":"200G"},{"name":"GRACE MACKEREL IN TOMATO SAUCE - CHUNKY","weight":"200G"},
+      {"name":"GRACE MACKEREL IN HOT TOMATO SAUCE - CHUNKY","weight":"200G"},{"name":"GRACE MACKEREL IN BRINE - CHUNKY","weight":"425G"},
+      {"name":"GRACE MACKEREL IN TOMATO SAUCE - CHUNKY","weight":"425G"},{"name":"GRACE MACKEREL IN HOT TOMATO SAUCE - CHUNKY","weight":"425G"},
+      {"name":"GRACE CHICKEN VIENNA SAUSAGES (HALAL)","weight":"200G"},{"name":"GRACE HOT DOG SAUSAGES CHICKEN (HALAL)","weight":"400G"},
+      {"name":"GRACE 100% HALAL CORNED BEEF","weight":"340G"}
+    ]
+  },
+  {
+    "name":"SUNSHINE SNACKS","slug":"sunshine-snacks","image":"/images/Ready.jpeg","products":[
+      {"name":"SUNSHINE CHEE ZEES ORIGINAL","weight":"45G"},{"name":"SUNSHINE CHEE ZEES ORIGINAL","weight":"225G"}
+    ]
+  },
+  {
+    "name":"DALGETY TEAS","slug":"dalgety-teas","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"GREEN TEA & GINGER","weight":""},{"name":"HONEY GINGER TEA","weight":""},{"name":"LEMON GINGER TEA","weight":""},
+      {"name":"PEPPERMINT & GINGER TEA","weight":""},{"name":"CERASSIE TEA","weight":""},{"name":"SLIMLINE TEA","weight":""},
+      {"name":"GOODNIGHT SLEEP TEA","weight":""},{"name":"CHAI MASALA TEA","weight":""},{"name":"NEEM TEA","weight":""},
+      {"name":"MANGO LEAF TEA","weight":""},{"name":"PURE GREEN TEA","weight":""},{"name":"PEPPERMINT","weight":""},
+      {"name":"LEMONGRASS/FEVERGRASS TEA","weight":""},{"name":"INSTANT GINGER TEA BOXES","weight":"126G"},
+      {"name":"INSTANT GINGER AND GINSENG TEA","weight":"126G"},{"name":"INSTANT GINGER TEA POUCHES","weight":"270G"},
+      {"name":"INSTANT LEMON AND GINGER POUCHES","weight":"270G"},{"name":"PURE MORINGA TEA","weight":""},
+      {"name":"MORINGA GREEN TEA","weight":""},{"name":"MORINGA PEPPERMINT TEA","weight":""},{"name":"MORINGA AND GINGER TEA","weight":""},
+      {"name":"PURE SOURSOP TEA","weight":""},{"name":"SOURSOP AND MORINGA TEA","weight":""},{"name":"SOURSOP AND GINGER TEA","weight":""},
+      {"name":"PURE TURMERIC TEA","weight":""},{"name":"TURMERIC & BLACK PEPPER TEA","weight":""},{"name":"TURMERIC & GINGER TEA","weight":""}
+    ]
+  }
+];
