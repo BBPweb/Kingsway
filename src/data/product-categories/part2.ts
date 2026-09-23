3e8db590952e0e516777188a6678fa88806cb599
BBPweb <art.bbp10@gmail.com>
2026-09-23 11:37:52 +0530

    Add catalogue product data part 2

---
+++ src/data/product-categories/part2.ts
@@ -0,0 +1,81 @@
+import type { ProductCategory } from './types';
+
+export const productCategoriesPart2: ProductCategory[] = [
+  {
+    "name":"SPICES","slug":"spices","image":"/images/Spiceswhole.jpeg","products":[
+      {"name":"AJWAIN SEED 300G","weight":"300G"},{"name":"AJWAIN SEED 700G","weight":"700G"},
+      {"name":"CUMIN WHOLE 300G","weight":"300G"},{"name":"CUMIN WHOLE 700G","weight":"700G"},
+      {"name":"CUMIN POWDER 100G","weight":"100G"},{"name":"CUMIN POWDER 400G","weight":"400G"},{"name":"CUMIN POWDER 1KG","weight":"1KG"},
+      {"name":"FENNEL SEED 300G","weight":"300G"},{"name":"FENNEL SEED 700G","weight":"700G"},
+      {"name":"MUSTARD SEED BLACK 1KG","weight":"1KG"},{"name":"MUSTARD SEED BLACK 400G","weight":"400G"},
+      {"name":"RED CHILLI POWDER 100G","weight":"100G"},{"name":"RED CHILLI POWDER 400G","weight":"400G"},{"name":"RED CHILLI POWDER 1KG","weight":"1KG"},
+      {"name":"SESAME SEED HULLED 1KG","weight":"1KG"},{"name":"SESAME SEED HULLED 400G","weight":"400G"},
+      {"name":"SESAME SEED NATURAL 1KG","weight":"1KG"},{"name":"SESAME SEED NATURAL 400G","weight":"400G"},
+      {"name":"KINGSWAY FENNEL SEED","weight":"25KG"},{"name":"KINGSWAY CUMIN SEED","weight":"25KG"},
+      {"name":"KINGSWAY AJWAIN SEED","weight":"25KG"},{"name":"KINGSWAY RED CHILLI POWDER","weight":"25KG"},
+      {"name":"KINGSWAY CUMIN POWDER","weight":"25KG"},{"name":"KINGSWAY SESAME SEED HULLED","weight":"25KG"},
+      {"name":"KINGSWAY SESAME SEED NATURAL","weight":"25KG"},{"name":"KINGSWAY MUSTARD SEED","weight":"25KG"}
+    ]
+  },
+  {
+    "name":"MILK POWDER","slug":"milk-powder","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"MAGGI COCONUT MILK POWDER","weight":"150G"},{"name":"MAGGI COCONUT MILK POWDER","weight":"300G"},
+      {"name":"MAGGI COCONUT MILK POWDER","weight":"1KG"},{"name":"ALPHAVIT MILK POWDER","weight":"25KG"}
+    ]
+  },
+  {
+    "name":"SUPARI | PAN MASALA","slug":"supari-pan-masala","image":"/images/Ethnicgroceryjpeg.jpeg","products":[
+      {"name":"KINGSWAY ARECANUT MOTI","weight":"25KG"},{"name":"KINGSWAY ARECANUT MORO","weight":"25KG"},
+      {"name":"KATHA UWP","weight":"100G"},{"name":"LACHA SUPARI","weight":"15KG"},
+      {"name":"RAJA CHUNA","weight":"100G"},{"name":"RAJA CHUNA","weight":"200G"},{"name":"RAJA CHUNA","weight":"1KG"},{"name":"GULKAND","weight":"800G"}
+    ]
+  },
+  {
+    "name":"FOOD COLOUR | ESSENCE | ADDITIVES","slug":"food-colour-essence-additives","image":"/images/Spicesground.jpeg","products":[
+      {"name":"PREEMA RED FOOD COLOUR","weight":"500G"},{"name":"PREEMA EGG YELLOW FOOD COLOUR","weight":"500G"},
+      {"name":"PREEMA LEMON YELLOW FOOD COLOUR","weight":"500G"},{"name":"PREEMA DEEP ORANGE FOOD COLOUR","weight":"500G"},
+      {"name":"TG KIAT ROSE SYRUP","weight":"750ML"},{"name":"ENO SALT REGULAR","weight":"100G"},
+      {"name":"AGAR AGAR POWDER","weight":"100G"},{"name":"AGAR AGAR POWDER","weight":"200G"},{"name":"AGAR AGAR POWDER","weight":"300G"},
+      {"name":"AGAR AGAR STRIPS","weight":"300G"},{"name":"JUMBO CHICKEN STOCK","weight":"1KG"},{"name":"JUMBO BEEF STOCK","weight":"1KG"},
+      {"name":"SWARNAM HING","weight":"50G"},{"name":"KINGSWAY HING","weight":"100G"},{"name":"KINGSWAY HING","weight":"500G"},
+      {"name":"KINGSWAY NATURAL TAMARIND","weight":"200G"},{"name":"KINGSWAY NATURAL TAMARIND","weight":"200G"},
+      {"name":"KINGSWAY NATURAL TAMARIND","weight":"500G"},{"name":"KINGSWAY NATURAL TAMARIND","weight":"1KG"},
+      {"name":"KINGSWAY BLACK TAMARIND","weight":"200G"}
+    ]
+  },
+  {
+    "name":"GRACE PLANTAIN CHIPS","slug":"grace-plantain-chips","image":"/images/Ready.jpeg","products":[
+      {"name":"GRACE SWEET PLANTAIN CHIPS","weight":"85G"},{"name":"GRACE GREEN PLANTAIN CHIPS","weight":"85G"}
+    ]
+  },
+  {
+    "name":"GRACE SAUCES & CONDIMENTS","slug":"grace-sauces-condiments","image":"/images/Ready.jpeg","products":[
+      {"name":"GRACE HOT PEPPER SAUCE","weight":"170ML"},{"name":"GRACE HOT PEPPER SAUCE - GARLIC","weight":"85ML"},
+      {"name":"GRACE JAMAICAN STYLE JERK BBQ SAUCE","weight":"375G"},{"name":"GRACE FISH & MEAT SAUCE","weight":"142ML"},
+      {"name":"GRACE BROWNING","weight":"142ML"}
+    ]
+  },
+  {
+    "name":"DUNN'S RIVER SPICES & SEASONINGS","slug":"dunns-river-spices-seasonings","image":"/images/Spicesground.jpeg","products":[
+      {"name":"DUNN'S RIVER ALL PURPOSE SEASONING","weight":"100G"},{"name":"DUNN'S RIVER EVERYDAY SEASONING","weight":"100G"},
+      {"name":"DUNN'S RIVER GROUND BLACK PEPPER","weight":"100G"},{"name":"DUNN'S RIVER CARRIBBEAN BARBECUE SEASONING","weight":"100G"},
+      {"name":"DUNN'S RIVER GROUND NUTMEG","weight":"100G"},{"name":"DUNN'S RIVER CHICKEN SEASONING","weight":"100G"},
+      {"name":"DUNN'S RIVER FISH SEASONING","weight":"100G"},{"name":"DUNN'S RIVER DRIED THYME","weight":"30G"},
+      {"name":"DUNN'S RIVER WHOLE PIMENTO","weight":"70G"},{"name":"DUNN'S RIVER HOT CURRY POWDER","weight":"100G"},
+      {"name":"DUNN'S RIVER MILD CURRY POWDER","weight":"100G"},{"name":"DUNN'S RIVER JERK SEASONING","weight":"100G"},
+      {"name":"DUNN'S RIVER TROPICAL SEASONING","weight":"100G"},{"name":"DUNN'S RIVER LAMB SEASONING","weight":"100G"},
+      {"name":"DUNN'S RIVER GARLIC SALT","weight":"70G"},{"name":"DUNN'S RIVER MIXED SPICE","weight":"70G"},
+      {"name":"DUNN'S RIVER COCK FLAVOURED SEASONING","weight":"100G"},{"name":"DUNN'S RIVER OXTAIL SEASONING","weight":"100G"},
+      {"name":"DUNN'S RIVER MIXED HERBS","weight":"30G"},{"name":"DUNN'S RIVER GROUND PIMENTO","weight":"80G"},
+      {"name":"DUNN'S RIVER GINGER GARLIC PIMENTO SEASONING","weight":"80G"},{"name":"DUNN'S RIVER PERI PERI SEASONING","weight":"100G"},
+      {"name":"DUNN'S RIVER BEEF & STEAK SEASONING","weight":"100G"},{"name":"DUNN'S RIVER LEMON JERK SEASONING","weight":"100G"},
+      {"name":"DUNN'S RIVER CHICKEN SEASONING","weight":"600G"},{"name":"DUNN'S RIVER DRIED THYME","weight":"200G"},
+      {"name":"DUNN'S RIVER HOT CURRY POWDER","weight":"500G"},{"name":"DUNN'S RIVER ALL PURPOSE SEASONING","weight":"700G"},
+      {"name":"DUNN'S RIVER MILD CURRY POWDER","weight":"500G"},{"name":"DUNN'S RIVER EVERYDAY SEASONING","weight":"700G"},
+      {"name":"DUNN'S RIVER BARBECUE SEASONING","weight":"600G"},{"name":"DUNN'S RIVER JERK SEASONING","weight":"650G"},
+      {"name":"DUNN'S RIVER MIXED HERBS","weight":"150G"},{"name":"DUNN'S RIVER FISH SEASONING","weight":"700G"},
+      {"name":"DUNN'S RIVER JAMAICAN JERK SEASONING","weight":"300G"},{"name":"DUNN'S RIVER JAMAICAN MILD JERK SEASONING","weight":"300G"},
+      {"name":"DUNN'S RIVER JAMAICAN GREEN SEASONING","weight":"300G"},{"name":"DUNN'S RIVER GOAT CURRY SEASONING","weight":"100G"}
+    ]
+  }
+];
