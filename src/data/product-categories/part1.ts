19a4d6a324f0ba8b9e4fb55598137879a9540eca
BBPweb <art.bbp10@gmail.com>
2026-09-23 11:37:22 +0530

    Add catalogue product data part 1

---
+++ src/data/product-categories/part1.ts
@@ -0,0 +1,174 @@
+import type { ProductCategory } from './types';
+
+export const productCategoriesPart1: ProductCategory[] = [
+  {
+    "name": "LENTILS",
+    "slug": "lentils",
+    "image": "/images/Pulseslentilsjpeg.jpeg",
+    "products": [
+      {"name":"BLACK EYE BEANS 500GM","weight":"500G"},
+      {"name":"BLACK EYE BEANS 1KG","weight":"1KG"},
+      {"name":"BLACK EYE BEANS 2KG","weight":"2KG"},
+      {"name":"BROWN MASOOR 500G","weight":"500G"},
+      {"name":"BROWN MASOOR 1KG","weight":"1KG"},
+      {"name":"BROWN MASOOR 2KG","weight":"2KG"},
+      {"name":"CHANA DAL 500GM","weight":"500G"},
+      {"name":"CHANA DAL 1KG","weight":"1KG"},
+      {"name":"CHANA DAL 2KG","weight":"2KG"},
+      {"name":"CHICK PEAS 500GM","weight":"500G"},
+      {"name":"CHICK PEAS 1KG","weight":"1KG"},
+      {"name":"CHICK PEAS 2KG","weight":"2KG"},
+      {"name":"MOONG WHOLE 500GM","weight":"500G"},
+      {"name":"MOONG WHOLE 1KG","weight":"1KG"},
+      {"name":"MOONG WHOLE 2KG","weight":"2KG"},
+      {"name":"KALA CHANA 500GM","weight":"500G"},
+      {"name":"KALA CHANA 1KG","weight":"1KG"},
+      {"name":"KALA CHANA 2KG","weight":"2KG"},
+      {"name":"RED KIDENY BEANS 500GM","weight":"500G"},
+      {"name":"RED KIDENY BEANS 1KG","weight":"1KG"},
+      {"name":"BABY RED KIDNEY BEANS 2KG","weight":"2KG"},
+      {"name":"BABY RED KIDNEY BEANS 1KG","weight":"500G"},
+      {"name":"BABY RED KIDNEY BEANS 500G","weight":"1KG"},
+      {"name":"RED SPLIT LENTILS 500GM","weight":"500G"},
+      {"name":"RED SPLIT LENTILS 1KG","weight":"1KG"},
+      {"name":"RED SPLIT LENTILS 2KG","weight":"2KG"},
+      {"name":"TOOR DAL PLAIN 500GM","weight":"500G"},
+      {"name":"TOOR DAL PLAIN 1KG","weight":"1KG"},
+      {"name":"TOOR DAL PLAIN 2KG","weight":"2KG"},
+      {"name":"URID DAL WASHED 1KG","weight":"1KG"},
+      {"name":"URID DAL WASHED 2KG","weight":"2KG"},
+      {"name":"URID WHOLE GOTA 500GM","weight":"500G"},
+      {"name":"URID WHOLE GOTA 1KG","weight":"1KG"},
+      {"name":"URID WHOLE GOTA 2KG","weight":"2KG"},
+      {"name":"MOONG DAL WASHED 500GM","weight":"500G"},
+      {"name":"MOONG DAL WASHED 1KG","weight":"1KG"},
+      {"name":"MOONG DAL WASHED 2KG","weight":"2KG"},
+      {"name":"KINGSWAY CHANA DAL","weight":"25KG"},
+      {"name":"KINGSWAY MASOOR DAL","weight":"25KG"},
+      {"name":"KINGSWAY CHICKPEAS","weight":"25KG"},
+      {"name":"KINGSWAY KALA CHANA","weight":"25KG"},
+      {"name":"KINGSWAY URID GOTA","weight":"25KG"},
+      {"name":"KINGSWAY URID WHOLE","weight":"25KG"},
+      {"name":"KINGSWAY URID DAL WASHED","weight":"25KG"},
+      {"name":"KINGSWAY YELLOW MOONG DAL","weight":"25KG"},
+      {"name":"KINGSWAY TOOR DAL PLAIN","weight":"25KG"},
+      {"name":"KINGSWAY GREEN MOONG BEANS","weight":"25KG"}
+    ]
+  },
+  {
+    "name": "GRACE PRODUCTS",
+    "slug": "grace-products",
+    "image": "/images/Ethnicgroceryjpeg.jpeg",
+    "products": [
+      {"name":"NURISHMENT ORIGINAL VANILLA","weight":"400G"},
+      {"name":"NURISHMENT ORIGINAL STAWBERRY","weight":"400G"},
+      {"name":"NURISHMENT ORIGINAL CHOCOLATE","weight":"400G"},
+      {"name":"NURISHMENT ORIGINAL BANANA","weight":"400G"}
+    ]
+  },
+  {
+    "name": "GRACE TROPICAL RYTHMS",
+    "slug": "grace-tropical-rythms",
+    "image": "/images/Ethnicgroceryjpeg.jpeg",
+    "products": [
+      {"name":"GRACE TROPICAL RHYTHMS MANGO CARROT","weight":"475ML"},
+      {"name":"GRACE TROPICAL RHYTHMS FRUIT PUNCH","weight":"475ML"},
+      {"name":"GRACE TROPICAL RHYTHMS SORREL GINGER","weight":"475ML"},
+      {"name":"GRACE TROPICAL RHYTHMS PINEAPPLE GINGER","weight":"475ML"}
+    ]
+  },
+  {
+    "name": "GRACE COCONUT WATER",
+    "slug": "grace-coconut-water",
+    "image": "/images/Ethnicgroceryjpeg.jpeg",
+    "products": [
+      {"name":"GRACE COCONUT WATER DRINK WITH COCONUT PIECES","weight":"310ML"}
+    ]
+  },
+  {
+    "name": "GRACE ALOEVERA DRINKS",
+    "slug": "grace-aloevera-drinks",
+    "image": "/images/Ethnicgroceryjpeg.jpeg",
+    "products": [
+      {"name":"GRACE SAY ALOE ORIGINAL FLAVOUR","weight":"500ML"},
+      {"name":"GRACE SAY ALOE MANGO FLAVOUR","weight":"500ML"},
+      {"name":"GRACE SAY ALOE STRAWBERRY FLAVOUR","weight":"500ML"},
+      {"name":"GRACE SAY ALOE LYCHEE FLAVOUR","weight":"500ML"}
+    ]
+  },
+  {
+    "name": "GRACE JERK & INGREDIENTS",
+    "slug": "grace-jerk-ingredients",
+    "image": "/images/Ready.jpeg",
+    "products": [
+      {"name":"GRACE SWEETENED CONDENSED MILK","weight":"397G"}
+    ]
+  },
+  {
+    "name": "DUNN'S RIVER INGREDIENTS",
+    "slug": "dunns-river-ingredients",
+    "image": "/images/Ethnicgroceryjpeg.jpeg",
+    "products": [
+      {"name":"DUNN'S RIVER JAMAICAN ACKEE","weight":"540G"},
+      {"name":"DUNN'S RIVER JAMAICAN CALLALOO","weight":"540G"},
+      {"name":"DUNN'S RIVER BUTTER BEANS","weight":"400G"},
+      {"name":"DUNN'S RIVER RED KIDNEY BEANS","weight":"400G"},
+      {"name":"DUNN'S RIVER BLACKEYE BEANS","weight":"400G"},
+      {"name":"DUNN'S RIVER CARIBBEAN PEAS & BEANS","weight":"400G"},
+      {"name":"DUNN'S RIVER GUNGO PEAS","weight":"400G"},
+      {"name":"DUNN'S RIVER GREEN PIGEON PEAS","weight":"425G"},
+      {"name":"DUNN'S RIVER BLACK BEANS","weight":"400G"},
+      {"name":"DUNN'S RIVER CREAMED COCONUT","weight":"200G"},
+      {"name":"DUNN'S RIVER JAMAICAN GUAVA JAM","weight":"340G"}
+    ]
+  },
+  {
+    "name": "FROZEN",
+    "slug": "frozen",
+    "image": "/images/Ready.jpeg",
+    "products": [
+      {"name":"SW-KOTHU POROTTA","weight":"454G"},
+      {"name":"SW-KERALA POROTTA (RESTAURANT PACK)","weight":"1.5KG"},
+      {"name":"SW-KERALA POROTTA (CATERING PACK)","weight":"908G"},
+      {"name":"SW-WHEAT POROTTA (FAMILY PACK)","weight":"908G"},
+      {"name":"SW-KERALA POROTTA (FAMILY PACK)","weight":"908G"},
+      {"name":"SW-GRATED COCONUT","weight":"400G"},
+      {"name":"SW-AVIAL MIX","weight":"400G"},
+      {"name":"SW-SAMBAR MIX","weight":"400G"},
+      {"name":"SW-DRUMSTICK","weight":"400G"},
+      {"name":"SW-MIXED VEGETABLE","weight":"400G"},
+      {"name":"SW-BANANA FRY","weight":"1KG / 454G"},
+      {"name":"SW-BANANA ROAST","weight":"1KG / 454G"},
+      {"name":"SW-UZHUNNUVADA","weight":"1KG / 454G"},
+      {"name":"SW-PARIPPU VADA","weight":"1KG / 454G"},
+      {"name":"SW-ELAYADA JACKFRUIT","weight":"454G"},
+      {"name":"SW-ELAYADA JAGGERY","weight":"454G"},
+      {"name":"SW-VEGETABLE CUTLET","weight":"1KG / 454G"},
+      {"name":"SW-UNNAKAYA","weight":"1KG / 454G"},
+      {"name":"SW-UNNIYAPPAM","weight":"1KG / 454G"},
+      {"name":"SW-NEYYAPPAM","weight":"1KG / 454G"},
+      {"name":"SW-WHITE GUAVA","weight":"500G"},
+      {"name":"SW-CUSTARD APPLE PULP","weight":"500G"},
+      {"name":"SW-ALPHONSO MANGO PULP","weight":"500G"},
+      {"name":"SW-ALPHONSO MANGO PULP","weight":"1KG"},
+      {"name":"SW-TENDER COCONUT PULP","weight":"1KG"},
+      {"name":"SW-PINK GUAVA PULP","weight":"1KG / 500G"},
+      {"name":"SW-CHICKOO PULP","weight":"1KG"},
+      {"name":"SW-SLICED COCONUT","weight":"400G"},
+      {"name":"SW-SHALLOTS","weight":"400G"},
+      {"name":"SW-GREEN JACKFRUIT SLICED","weight":"400G"},
+      {"name":"SW-SLICED ONION","weight":"1KG"},
+      {"name":"SW-TAPIOCA SLICED (CATERING PACK)","weight":"2KG"},
+      {"name":"SW-TAPIOCA SLICED","weight":"908G"},
+      {"name":"SW-NOOL POROTTA (CATERING PACK)","weight":"1KG"},
+      {"name":"SW-SUGHIYAN (CATERING PACK)","weight":"1KG"},
+      {"name":"SW-PATHIRI","weight":"454G"},
+      {"name":"SW-RUMALI ROTI","weight":"454G"},
+      {"name":"SW-IDLI (CATERING PACK)","weight":"1KG"},
+      {"name":"SW-IDIYAPPAM BROWN","weight":"454G"},
+      {"name":"SW-IDIYAPPAM WHITE (CATERING PACK)","weight":"1KG"},
+      {"name":"SW-IDIYAPPAM CHEMBA (CATERING PACK)","weight":"1KG"},
+      {"name":"SW-PALAPPAM (CATERING PACK)","weight":"1KG"}
+    ]
+  }
+];
